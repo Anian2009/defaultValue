738d4c5337dd31188c29738ded7094414e065812
Anian2009 <Anian2009@ukr.net>
2019-04-18 18:05:56 +0300

    Initial commit

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    console.log("hello index.js");
+
+    var  sliderIndex = 1;
+
+    $('.menu-button').click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('menu-button_active');
+        $('.navigation').toggleClass('navigation_active');
+        $('.cont').toggleClass('cont-active');
+    });
+
+    $('.previous').click(function () {
+        showSlides(sliderIndex+=-1);
+    });
+
+    $('.next').click(function () {
+        showSlides(sliderIndex+=1);
+    });
+
+
+    showSlides(sliderIndex);
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName('slides');
+
+        if (n>slides.length){
+            sliderIndex=1;
+        }
+        if (n<1){
+            sliderIndex = slides.length;
+        }
+        for(i=0; i<slides.length;i++){
+            slides[i].style.display = 'none';
+        }
+        slides[sliderIndex-1].style.display = "block";
+    }
+});
